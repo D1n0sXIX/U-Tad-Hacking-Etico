b6e86640588e018ad7df000179364ea2b8e30cd6
D1n0sXIX <alej.mlm03@gmail.com>
2026-03-04 20:15:15 +0100

    Obtencion de aprametros GET en URL

--- Practicas/Practica-1-XSS-SQLi/d1n0-xss-scanner/modulos/DetectorParametrosGet.py
+++ Practicas/Practica-1-XSS-SQLi/d1n0-xss-scanner/modulos/DetectorParametrosGet.py
@@ -2,6 +2,7 @@
 
 # booleano que indica si hay parametros GET en la URL
 def hay_parametros_get(url):
+    # Comprobamos si la URL contiene un "?" que indica el inicio de los parametros GET
     if "?" in url:
         print("Se han detectado parametros GET en la URL")
         return True
@@ -13,21 +14,19 @@ def hay_parametros_get(url):
 def separar_parametros_get(url):
     # Creamos un diccionario para almacenar los parametros --> clave: valor
     parametros = {}
-    # Si la URL tiene parametros GET
-    if "?" in url:
-        # Separamos la parte de la URL
-        partes = url.split("?")
-        # Si hay una algo despues del "?"
-        if len(partes) > 1:
-            # Los separamos por "&" y luego por "=" para obtener clave y valor
-            parametros_str = partes[1]
-            pares = parametros_str.split("&")
-            # Para cada par clave-valor, los separamos por "=" y los almacenamos en el diccionario
-            for par in pares:
-                # Si el par contiene un "=", lo separamos en clave y valor, sino lo consideramos como una clave sin valor
-                if "=" in par:
-                    clave, valor = par.split("=", 1)
-                    parametros[clave] = valor
+    # Separamos la parte de la URL
+    partes = url.split("?")
+    # Si hay una algo despues del "?"
+    if len(partes) > 1:
+        # Los separamos por "&" y luego por "=" para obtener clave y valor
+        parametros_str = partes[1]
+        pares = parametros_str.split("&")
+        # Para cada par clave-valor, los separamos por "=" y los almacenamos en el diccionario
+        for par in pares:
+            # Si el par contiene un "=", lo separamos en clave y valor, sino lo consideramos como una clave sin valor
+            if "=" in par:
+                clave, valor = par.split("=", 1)
+                parametros[clave] = valor
     return parametros
 # Ejemplo de diccionario resultado: {"id": "123", "search": "test"} <--> http://example.com/page?id=123&search=test
 
@@ -35,7 +34,7 @@ def separar_parametros_get(url):
 def parsear_parametros_URL(response):
     # Extrae la URL de la respuesta
     url = response.url
-    # Primero comprobamos si hay parametros GET en la URL, si no los hay, devolvemos un diccionario vacio
+    # Primero validamos si hay parametros GET en la URL, si no los hay, devolvemos un diccionario vacio
     if not hay_parametros_get(url):
         return {}
     parametros = separar_parametros_get(url)
